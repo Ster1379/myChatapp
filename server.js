bfabf808b6b6350b70b99001b4a5689466faa666
Ster1379 <rstreeter1952@gmail.com>
2023-06-20 18:07:42 -0400

    first update

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const path = require("path")
+var app = express()
+var server = app.listen(5056, function(){
+    console.log("Listen on port 5056")
+})
+
+const io = require("socket.io")(server)
+app.use(express.static(path.join(__dirname,"")))
+
+var userConnections = []
+io.on("connection",(socket)=>{
+    console.log('socket id is: ', socket.id)
+    socket.on("userconnect", (data) =>{
+        //console.log("userconnect ", data.displayName, data.meetingid)
+        var other_users = userConnections.filter((p) => p.meeting_id === data.meetingid)
+        userConnections.push({
+            connectionId: socket.id,
+            user_id: data.displayName,
+            meeting_id: data.meetingid,
+        })
+
+        other_users.forEach((v) => {
+            socket.to(v.connectionId).emit("inform_others_about_me", {
+                other_user_id: data.displayName,
+                connId: socket.id,
+            })
+        })
+
+        socket.emit("inform_me_about_other_user", other_users)
+    })
+    socket.on("SDPProcess", (data) => {
+        console.log("sdp process",  data.to_connid)
+        socket.to(data.to_connid).emit("SDPProcess", {
+            message: data.message,
+            from_connid: socket.id,
+        })
+    })
+
+    socket.on("disconnect", function(){
+        console.log('User disconnected')
+        let disUser = userConnections.find((p) => p.connectionId === socket.id)
+        if(disUser){
+            let meetingid = disUser.meeting_id
+            userConnections = userConnections.filter((p) => p.connectionId !== socket.id)
+            let list = userConnections.filter((p) => p.meeting_id === meetingid)
+            list.forEach((v) => {
+                socket.to(v.connectionId).emit("inform_other_about_disconnected_user",{
+                    connId: socket.id,
+                })
+            })
+        }
+    })
+})
